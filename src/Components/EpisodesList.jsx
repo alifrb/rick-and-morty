0d93bae039f9d32f50dd852960b4fafe146aef33
alifrb <ali.frh92@gmail.com>
2024-03-25 20:02:23 +0330

    episode

--- src/Components/EpisodesList.jsx
+++ src/Components/EpisodesList.jsx
@@ -7,7 +7,7 @@ const EpisodesList = ({ episodes, selectedCharacterId }) => {
 
   if (selectedCharacterId === null || episodes === null) {
     return;
-  }  
+  }
 
   return (
     <div className="row w-100 episodes-list-section">
@@ -27,6 +27,8 @@ const EpisodesList = ({ episodes, selectedCharacterId }) => {
 };
 
 export default EpisodesList;
+
+//
 
 const Episode = ({ episode }) => {
   return (
